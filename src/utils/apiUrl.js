f397206f5fc55733e699884ece67b6ac7277b0ea
teshe <teshomeabebe114@gmail.com>
2025-08-16 15:17:26 +0300

    fix: Update API_URL to point to the new production backend; comment out local development URL

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000'
-// export const API_URL = 'https://next-spin-backend.onrender.com';
+//export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://spin-backend-zcjq.onrender.com';
